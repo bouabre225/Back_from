f01b92dd2d8bb2747fea29c90ba90e657eec96bf
bouabre225 <angebouabre463@gmail.com>
2026-02-10 18:13:43 +0100

    fix: modification

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 
 require_once './User.php';
 
+// CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
     exit;
 }
+
 
 $data = json_decode(file_get_contents('php://input'), true);
 if (!$data) $data = $_POST;
